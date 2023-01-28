346deaa8def7654081be9037e6ab334a7cb704c7
Shodmon-creator <mamayusupovshodmon2003@gmail.com>
2023-01-28 21:01:26 +0500

    Task has been done

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,27 @@
 package com.epam.mjc.nio;
-
-import java.io.File;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        Map<String, String> map = null;
+        try {
+            String data = Files.readString(Paths.get(file.getPath()));
+            map = data.lines().map(s -> s.split(":"))
+                    .collect(Collectors.toMap(a -> a[0].trim(), a -> a[1].trim()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert map != null;
+        return new Profile(map.get("Name"), Integer.parseInt(map.get("Age")), map.get("Email"),
+                Long.parseLong(map.get("Phone")));
     }
+
+
 }
